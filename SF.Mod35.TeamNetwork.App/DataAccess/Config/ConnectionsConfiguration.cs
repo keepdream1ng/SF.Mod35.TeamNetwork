0b55687f90675a10ac673afe7070c0d7add57f39
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-01 05:52:21 +0530

    uow, connectionsRepository work

--- SF.Mod35.TeamNetwork.App/DataAccess/Config/ConnectionsConfiguration.cs
+++ SF.Mod35.TeamNetwork.App/DataAccess/Config/ConnectionsConfiguration.cs
@@ -11,5 +11,18 @@ public class ConnectionsConfiguration : IEntityTypeConfiguration<Connection>
 		builder.ToTable("UsersConnections").HasKey(p => p.Id);
 		builder.Property(x => x.Id).UseIdentityColumn();
 		builder.Property(x => x.Status).IsRequired().HasConversion<byte>();
+
+        // SQL keept yelling on me about cascade cicles till i configured this.
+        builder
+            .HasOne<User>(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.ClientNoAction);
+
+        builder
+            .HasOne<User>(x => x.ConnectedUser)
+            .WithMany()
+            .HasForeignKey(x => x.ConnectedUserId)
+            .OnDelete(DeleteBehavior.ClientNoAction);
 	}
 }
