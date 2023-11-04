64ea9ed2f6fbf694490c85dbcf8d81e9ed751d37
Anatolii Ivanov <gotnickname@gmail.com>
2023-11-04 13:55:34 +0530

    added generate route for testing

--- SF.Mod35.TeamNetwork.App/Controllers/SearchController.cs
+++ SF.Mod35.TeamNetwork.App/Controllers/SearchController.cs
@@ -12,60 +12,71 @@ namespace SF.Mod35.TeamNetwork.App.Controllers;
 
 public class SearchController : Controller
 {
-	private IMapper _mapper;
-	private readonly UserManager<User> _userManager;
-	private readonly SignInManager<User> _signInManager;
-	private readonly IUnitOfWork _unitOfWork;
+    private IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    private readonly IUnitOfWork _unitOfWork;
 
-	public SearchController(
-		UserManager<User> userManager,
-		SignInManager<User> signInManager,
-		IUnitOfWork unitOfWork,
-		IMapper mapper)
-	{
-		_userManager = userManager;
-		_signInManager = signInManager;
-		_unitOfWork = unitOfWork;
-		_mapper = mapper;
-	}
+    public SearchController(
+        UserManager<User> userManager,
+        SignInManager<User> signInManager,
+        IUnitOfWork unitOfWork,
+        IMapper mapper)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
 
-	[Route("UserList")]
-	[HttpPost]
-	public async Task<IActionResult> UserList(string search)
-	{
-		SearchViewModel model = await CreateSearch(search);
-		return View("SearchView", model);
-	}
+    [Route("UserList")]
+    [HttpPost]
+    public async Task<IActionResult> UserList(string search)
+    {
+        SearchViewModel model = await CreateSearch(search);
+        return View("SearchView", model);
+    }
 
-	private async Task<SearchViewModel> CreateSearch(string search)
-	{
-		var currentUser = await _userManager.GetUserAsync(User);
-		List<User> userSearch = _userManager.Users
-				.AsEnumerable()
-				.Where(x => x.FirstName.ToLower().Contains(search.ToLower()))
-				.ToList();
-		var model = new SearchViewModel();
-		// If current user is not logged in - he wont get an option to add connections.
-		if(currentUser == null)
-		{
-			model.UserList = userSearch.Select(u =>
-				new Tuple<User, ConnectionStatus>(u, ConnectionStatus.Unknown)).ToList();
-		}
-		else
-		{
-			model.UserList = userSearch.Select(u =>
-				new Tuple<User, ConnectionStatus>(u, GetConnectionStatus(currentUser, u))).ToList();
-		}
-		return model;
-	}
+    private async Task<SearchViewModel> CreateSearch(string search)
+    {
+        var currentUser = await _userManager.GetUserAsync(User);
+        List<User> userSearch;
+        if (!string.IsNullOrEmpty(search))
+        {
+            userSearch = _userManager.Users
+                    .AsEnumerable()
+                    .Where(x => x.FirstName.ToLower().Contains(search.ToLower()))
+                    .ToList();
+        }
+        else
+        {
+            // One of the task was to make emty search give all users from db.
+            userSearch = _userManager.Users
+                    .AsEnumerable()
+                    .ToList();
+        }
+        var model = new SearchViewModel();
+        // If current user is not logged in - he wont get an option to add connections.
+        if (currentUser == null)
+        {
+            model.UserList = userSearch.Select(u =>
+                new Tuple<User, ConnectionStatus>(u, ConnectionStatus.Unknown)).ToList();
+        }
+        else
+        {
+            model.UserList = userSearch.Select(u =>
+                new Tuple<User, ConnectionStatus>(u, GetConnectionStatus(currentUser, u))).ToList();
+        }
+        return model;
+    }
 
-	private ConnectionStatus GetConnectionStatus(User currentUser, User target)
-	{
-		if (currentUser.Id == target.Id)
-		{
-			return ConnectionStatus.Perfect;
-		}
-		return _unitOfWork.ConnectionsRepo.GetConnectionStatus(currentUser, target);
-	}
+    private ConnectionStatus GetConnectionStatus(User currentUser, User target)
+    {
+        if (currentUser.Id == target.Id)
+        {
+            return ConnectionStatus.Perfect;
+        }
+        return _unitOfWork.ConnectionsRepo.GetConnectionStatus(currentUser, target);
+    }
 }
 
--- SF.Mod35.TeamNetwork.App/Controllers/TestingController.cs
+++ SF.Mod35.TeamNetwork.App/Controllers/TestingController.cs
@@ -7,6 +7,7 @@ using SF.Mod35.TeamNetwork.ClassLibrary.Models;
 using SF.Mod35.TeamNetwork.App.Views.Search;
 using SF.Mod35.TeamNetwork.App.DataAccess.Repository;
 using SF.Mod35.TeamNetwork.App.DataAccess.UoW;
+using SF.Mod35.TeamNetwork.Testing;
 
 namespace SF.Mod35.TeamNetwork.App.Controllers;
 
@@ -16,25 +17,43 @@ namespace SF.Mod35.TeamNetwork.App.Controllers;
 /// </summary>
 public class TestingController : Controller
 {
-	private readonly UserManager<User> _userManager;
+    private readonly UserManager<User> _userManager;
+    private readonly DataGenerator _data = new DataGenerator();
+    private readonly string _testPassword = "123456";
 
-	public TestingController(UserManager<User> userManager)
-	{
-		_userManager = userManager;
-	}
+    public TestingController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
 
-	[Route("TestingEnviroment")]
-	[HttpGet]
-	public async Task<IActionResult> TestingEnviroment(string search)
-	{
-		if (IsTestingEnabled())
-		{
+    [Route("TestingEnviroment")]
+    [HttpGet]
+    public async Task<IActionResult> TestingEnviroment(string search)
+    {
+        if (IsTestingEnabled())
+        {
             return View("TestingEnviroment");
-		}
+        }
         return RedirectToAction("Index", "Home");
-	}
+    }
 
-	private bool IsTestingEnabled()
+    [Route("Generate")]
+    [HttpGet]
+    public async Task<IActionResult> Generate()
+    {
+        if (IsTestingEnabled())
+        {
+            List<User> testUsers = _data.UserFaker.Generate(35);
+            foreach (var user in testUsers)
+            {
+                var result = await _userManager.CreateAsync(user, _testPassword);
+            }
+            return RedirectToAction("TestingEnviroment", "Testing");
+        }
+        return RedirectToAction("Index", "Home");
+    }
+
+    private bool IsTestingEnabled()
     {
         string enableTestController = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
         // Return true if the environment variable is set to "true"
